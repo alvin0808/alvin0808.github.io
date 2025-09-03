0d94f1fefea24c3788302edd998580423abc49ba
alvin0808 <155709997+alvin0808@users.noreply.github.com>
2025-09-03 16:17:32 +0000

    Deploying to gh-pages from @ alvin0808/alvin0808.github.io@d41cf60742325feeff7aa6a38918b9d39243a6b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,6 +429,16 @@ ninja.data = [{
           description: "SVRaster Geometry Refinement via SDF Learning and NeuS-style Volume Rendering",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-bulnabi-software-club",
+          title: 'Bulnabi Software Club',
+          description: "Hands-on activities in drone and robotics software",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-optimal-beam-design-project",
+          title: 'Optimal Beam Design Project',
+          description: "Term project in Materials and Manufacturing Processes - CFRP-Woodrock composite beam design and testing",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
